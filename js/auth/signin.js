da32d68f0702868c4454a127713827b812aff164
MrLeoufff <mrleoufff@gmail.com>
2024-03-19 17:54:07 +0100

    ajout affichage et masquage des éléments de connection

--- js/auth/signin.js
+++ js/auth/signin.js
@@ -8,11 +8,12 @@ function checkCredentials() {
     // Ici il faudra appeller l'API pour vérifier les credentials en BDD
 
     if (mailInput.value == "reneleliard@gmail.com" && passwordInput.value == "Renstel#84") {
-        alert ("Vous etes co")
+        // alert ("Vous etes co")
         // Il faudra récupéré le vrai tocken
-        const tocken = "qsdfghqsdfghqsdfghqsdfghqsdfgh";
-
-        // placer le tocken en cookie
+        const token = "qsdfghqsdfghqsdfghqsdfghqsdfgh";
+        setToken(token);
+        // Placer le tocken en cookie
+        setCookie(roleCookieName, "admin", 7);
 
         window.location.replace("/");
     } else {
